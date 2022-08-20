43dd73b643cb2d427c1de125fd5450fd67a58335
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-08-20 19:15:11 +0300

    Add my model

--- application/main.py
+++ application/main.py
@@ -2,18 +2,21 @@ from fastapi import FastAPI
 from application.classes import Response, Request, SingleResponse
 import pandas as pd
 import pickle
+import re
+
 
 app = FastAPI(description='API для сопоставления товаров с их эталонами')
+with open('data/tf_idf_model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 
 @app.post('/get_results')
 async def get_results(message: Request):
     items = [[item.id, item.name, item.props] for item in message.items]
     df = pd.DataFrame(items, columns=['id', 'name', 'props'])
-    index = pd.DataFrame(df['id']).values.tolist()
+    index = df['id'].values.tolist()
+    print(index[0], type(index[0]))
 
-    with open('data/tf_idf_model.pkl', 'r') as file:
-        model = pickle.load(file)
     texts = preprocess_data(df)
     predictions = model.predict(texts)
     predictions = predictions.tolist()
@@ -27,3 +30,4 @@ def preprocess_data(df):
     df['text'] = df['name'] + '. ' + df['string_props']
     df['text'] = df['text'].apply(lambda x: x.replace('\t', ' '))
     return df['text']
+
---
+++ local_run.sh
@@ -0,0 +1,1 @@
+uvicorn application.main:app --reload
