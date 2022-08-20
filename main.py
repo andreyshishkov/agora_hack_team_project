7bd7a349d1159c5c948fbf0eed2ca2b6f6ee6bf5
Denis <aveocr@yandex.ru>
2022-08-20 17:45:26 +0400

    Removed brakets from return by deal_post_data.
    Add construction if __name__ == '__main__' and new function main. We'll can excepting error and incorrect working program in different system.
    
    Removed libs don't used in code

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[698]:
-
-
-from distutils.command.upload import upload
-import pandas as pd
-import numpy as np
-import json
-
-import string
-
-from tqdm import tqdm
-tqdm.pandas()
-
-
-# In[699]:
-
-
-from tensorflow.keras.models import Sequential, load_model, save_model
-from tensorflow.keras.layers import Dense, Embedding, MaxPooling1D, Conv1D, GlobalMaxPooling1D, Dropout, LSTM, GRU
-from tensorflow.keras import utils
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer, tokenizer_from_json
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras import utils
-
-import matplotlib.pyplot as plt
-
 import http.server
 import socketserver
 import io
@@ -36,9 +8,10 @@ from identify import identify
 
 PORT = 8100
 
+
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 
-    def do_POST(self):        
+    def do_POST(self):
         r, info = self.deal_post_data()
         print(r, info, "by: ", self.client_address)
         f = io.BytesIO()
@@ -56,28 +29,36 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         if f:
             self.copyfile(f, self.wfile)
-            f.close()      
+            f.close()
 
     def deal_post_data(self):
         ctype, pdict = cgi.parse_header(self.headers['Content-Type'])
         pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
         pdict['CONTENT-LENGTH'] = int(self.headers['Content-Length'])
         if ctype == 'multipart/form-data':
-            form = cgi.FieldStorage( fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'], })
-            print (type(form))
+            form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,
+                                    environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE': self.headers['Content-Type'], })
+            print(type(form))
             try:
                 if isinstance(form["file"], list):
                     for record in form["file"]:
-                        open("./%s"%record.filename, "wb").write(record.file.read())
+                        open("./%s" % record.filename, "wb").write(record.file.read())
                 else:
-                    open("./%s"%form["file"].filename, "wb").write(form["file"].file.read())
+                    open("./%s" % form["file"].filename, "wb").write(form["file"].file.read())
             except IOError:
-                    return (False, "Can't create file to write, do you have permission to write?")
-        
-        
-        return (True, identify())
+                return False, "Can't create file to write, do you have permission to write?"
 
-Handler = CustomHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+        return True, identify()
+
+
+def main():
+    Handler = CustomHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port" , PORT)
+        httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
+else:
+    print("It's file not library and executable file!")
